1b31414bd57356e416436aab546f9d72fe224776
Shavin <41513267+shxviin@users.noreply.github.com>
2019-08-09 01:47:38 +0530

    Add Medicine Pop Up

---
+++ app/src/main/java/com/example/pharmeasy/AddMedicineActivity.java
@@ -0,0 +1,22 @@
+package com.example.pharmeasy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class AddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medicine);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.3));
+    }
+}
--- app/src/main/java/com/example/pharmeasy/MedicineActivity.java
+++ app/src/main/java/com/example/pharmeasy/MedicineActivity.java
@@ -1,10 +1,14 @@
 package com.example.pharmeasy;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.pharmeasy.Adapter.MedicineAdapter;
 import com.example.pharmeasy.Model.Medicine;
@@ -30,6 +34,15 @@ public class MedicineActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
+
+        FloatingActionButton button_medicine = findViewById(R.id.addMedFloatingButton);
+        button_medicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent add_medicine_intent = new Intent( MedicineActivity.this, AddMedicineActivity.class);
+                startActivity(add_medicine_intent);
+            }
+        });
 
         initOrdersData();
     }
