31954614eef4b6a2f1fde0bc4aaee1f6178d9ee8
Praveen Nithiyananda <n.praveen28j@gmail.com>
2019-08-09 16:15:32 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/pharmeasy/HospitalActivity.java
    #	app/src/main/res/menu/activity_hospital_drawer.xml
    #	app/src/main/res/values/drawables.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/pharmeasy/PatientsActivity.java
@@ -0,0 +1,13 @@
+package com.example.pharmeasy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patients);
+    }
+}
