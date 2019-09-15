98745c5b16d6145e2de632872ec8405b77bcbaa2
sameeratse <sameeratse@gmail.com>
2019-09-15 19:14:28 +0530

    feedback interface

--- app/src/main/java/com/example/pharmeasy/pharmFeedback.java
+++ app/src/main/java/com/example/pharmeasy/pharmFeedback.java
@@ -3,11 +3,11 @@ package com.example.pharmeasy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class feedbackActivity extends AppCompatActivity {
+public class pharmFeedback extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feedback);
+        setContentView(R.layout.activity_pharm_feedback);
     }
 }
